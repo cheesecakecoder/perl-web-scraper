5768c6cff2938a68c8f34be756fb5059f385b7ae
Tom Chmielarz <tom.chmielarz@gmail.com>
2018-05-16 14:01:26 -0400

    Create web_scrape_title.pl

---
+++ web_scrape_title.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use LWP::UserAgent;
+
+my $request_url = "http://webscrapingscripts.com/";
+
+my $extract_from = '<title>';
+my $extract_to = '</title>';
+my $title = '';
+
+my $req = HTTP::Request->new(GET => $request_url);
+$req->header("User-Agent" => "Perl Scraper 1.0");
+
+my $ua = LWP::UserAgent->new;
+my $response = $ua->request($req);
+my $respContent = $response->{_content};
+
+if ( (my $from_index = index($respContent, $extract_from)) != -1){
+	print $from_index;
+	if ( (my $to_index = index($respContent, $extract_to)) != -1){
+		print "To index = $to_index <br>\r\n";
+		$title = substr($respContent, $from_index+length($extract_from),
+						$to_index-$from_index-length($extract_from));
+		printf("Title is %s\r\n", $title);
+		#
+		#TODO do something with the data
+		#
+	}else{
+		print "Error: extract_to string not found in response.\r\n";
+	}
+}else{
+	print "Error: extract_from string not found in response.\r\n";
+}
+
